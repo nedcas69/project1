e994582c3653cec58f8bd2095918847b1cecd968
nedcas69 <nedcas69@gmail.com>
2022-10-04 12:29:06 +0500

    master

--- hello.py
+++ hello.py
@@ -4,7 +4,16 @@ from ast import In
 print('hello world - привет мир!!')
 
 x = 2
+print(x)
+
+x = (x + x) * x + 15 
+print(x)
+type(x)
 
 x = ['slime', 'bar']
+print(x[0], x[1])
+type(x)
 
+x = 30.2
+type(x)
 print(x)
