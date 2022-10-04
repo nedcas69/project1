3c55cabcb19375bf34728fd27856e4768ee542d6
nedcas69 <nedcas69@gmail.com>
2022-10-04 16:24:10 +0500

    master

--- string_metods.py
+++ string_metods.py
@@ -1,8 +1,23 @@
 name = "Sam"
 #name[0] = "P" не будет работать так как строки не изменны
 last_letters = name [1:]
-print('P' + last_letters)# конкатенация строк!!!
+print('P' + last_letters) #конкатенация строк!!!
 
 x = 'Hello world'
-x = x + ' it is beautiful outside!'
+x = x + ' it is beautiful outside!\n'
 print(x)
+x = x.upper()  #заглавные буквы/ lower() все буквы в нижний регистр!
+#x = x.split() # разбивает строку на отдельные части. 
+# поумолчанию стоить пробел, можно поменять значение
+# разделителя на присуствующие в строке буквы или символы
+letter = x * 9 # умножение строк
+
+print(letter)
+
+# 2 + 3 = 5
+# '2' + '3' = '23'
+ 
+
+my_name = ' Shax'
+hello = 'Hello'
+print(hello + my_name)
