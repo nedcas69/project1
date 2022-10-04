99b5babeb7c3d9adb07955c19b8959eee1513b44
nedcas69 <nedcas69@gmail.com>
2022-10-04 14:16:19 +0500

    master

---
+++ string.py
@@ -0,0 +1,19 @@
+print('hello')
+print("world")
+print("hello world \t\ni'm programmer ")
+
+len('hello world')
+my_string = 'Hello world'
+print(my_string[0], my_string[4], my_string[6], my_string[-2], my_string[-1])
+
+#slicing
+my_string = 'qwertyuiopasdfghjklzxcvbnm'
+print(len(my_string))
+print(my_string[5:])  
+print(my_string[:5])
+print(my_string[5:15])
+print(my_string[::2]) #третий параметр размер шага qetuoadgjlxvn
+print(my_string[::-1]) # инверсия строки!
+
+my_string = 'йцукенгшщзхъфывапролджэячсмитьбюё'
+print(len(my_string))
---
+++ string_metods.py
@@ -0,0 +1,8 @@
+name = "Sam"
+#name[0] = "P" не будет работать так как строки не изменны
+last_letters = name [1:]
+print('P' + last_letters)# конкатенация строк!!!
+
+x = 'Hello world'
+x = x + ' it is beautiful outside!'
+print(x)
