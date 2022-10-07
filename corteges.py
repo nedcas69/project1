6fb215682651b6f8c2ba56d4b4981e5891431867
nedcas69 <nedcas69@gmail.com>
2022-10-07 12:07:17 +0500

    m

---
+++ corteges.py
@@ -0,0 +1,8 @@
+cort = (1,2,3) # кортеджи неизменны
+my_list = [1,2,3]
+
+print(len(cort))
+
+cort = ('a', 'a', 'b')
+print(cort.count('a'))
+print(cort.index('a'))
---
+++ sets.py
@@ -0,0 +1,9 @@
+my_set = set() #pustoe mnojestva 
+my_set.add(1) # dobavleniya chisla 1
+my_set.add(2) #znacheniya bolwe odnovo raza dobavlyat nelzya
+my_list = [1,1,1,1,1,2,2,2,2,4,3,3,3,4,4,4,5,]
+
+new_set = set(my_list)
+print(my_list)
+print(new_set)
+print(my_set)
