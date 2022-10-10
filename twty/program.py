300604b3dcaa358009beb4b714f1de436c13e429
nedcas69 <nedcas69@gmail.com>
2022-10-10 15:34:13 +0500

    vot_te

---
+++ twty/program.py
@@ -0,0 +1,11 @@
+from turtle import *
+speed(100)
+colors = ['black', 'red', 'black', 'red']
+bgcolor('white')
+
+for i in range(1000):
+    pencolor(colors[i%4])
+    width(i//100+1)
+    forward(i)
+    left(100)
+done
