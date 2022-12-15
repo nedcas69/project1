5edd5fe659a8377993d8ae720c0fb710e10fd3ac
nedcas69 <nedcas69@github.com>
2022-12-15 12:55:54 +0500

    fff

---
+++ webformyself/cycle1.py
@@ -0,0 +1,14 @@
+# i = 1900
+
+# while i <2023:
+#     print(i, end=' ')
+#     i += 1
+# else:
+#     print('\n')
+     
+# таблица умножений
+for i in range(1,10):
+    for j in range(2,10):
+        print(f'{i} * {j} = {i*j}\t', end=' ')
+    
+    print('') 
